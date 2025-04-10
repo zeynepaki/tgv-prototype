280b39289775348df3f648d6be72f3ba27d303b6
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2025-04-10 11:51:39 +0100

    Make upstream address for typesense server configurable via environment ($TYPESENSE_HOST)

--- 010-populate_config_template.sh
+++ 010-populate_config_template.sh
@@ -1,1 +1,5 @@
+# Interpolate environment variables:
+#   ... in the web front end:
 envsubst < /usr/share/nginx/html/config.template.js > /usr/share/nginx/html/config.js
+#   ... and in the nginx configuration:
+envsubst < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
