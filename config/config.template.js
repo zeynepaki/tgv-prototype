16c9038de276bb75a2d9f66e09cba2797aef8301
zeynepaki <zeynep.aki@durham.ac.uk>
2025-05-09 13:36:38 +0100

    Update config.template.js

--- config/config.template.js
+++ config/config.template.js
@@ -1,10 +1,10 @@
 const TYPESENSE_CLIENT_CONFIG = {
     nodes: [{
-      host: 'localhost',
-      port: '80',
-      protocol: 'http',
-      path: '/api',
+        host: '${TYPESENSE_HOST}',
+        port: '${TYPESENSE_PORT}',
+        protocol: '${TYPESENSE_PROTOCOL}',
+        path: '${TYPESENSE_PATH}',
     }],
     apiKey: '${TYPESENSE_API_KEY}',
     connectionTimeoutSeconds: 2
-  };
+};
