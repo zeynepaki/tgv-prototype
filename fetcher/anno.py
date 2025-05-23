4501754e12bb6b1a08a8867f91ab69babe272d51
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2025-05-23 16:33:30 +0100

    Alter data source scripts to conform to a standard object definition

--- fetcher/anno.py
+++ fetcher/anno.py
@@ -1,44 +1,96 @@
-import argparse
+from datasource import DataSource
+from typing import List
+
 import logging
 import os
 
-import requests_cache
 import tqdm
 
 import utils 
 
 logging.basicConfig(level=logging.INFO)
 
-SOURCE_ID = "anno.onb.ac.at"
-BASE_URL = "https://anno.onb.ac.at"
-TEXT_URL = "https://anno.onb.ac.at/cgi-content/annoshow?text={title_id}|{datum}|{page_number}"
-IMAGE_URL= "https://anno.onb.ac.at/cgi-content/annoshow?call={title_id}|{datum}|{page_number}|{zoom_level}"
+class AnnoDataSource(DataSource):
+    """
+    Data source for fetching annotations from the Austrian National Library (ONB) (Austrian Newspapers Online) IIIF service.
+    """
 
-def get_valid_datums(title_id, session):
-    TITLE_BASE = "/cgi-content/anno?apm=0&aid={}".format(title_id)
-    title_url = BASE_URL + TITLE_BASE
-    all_hrefs = utils.get_all_hrefs(title_url, session=session)
-    year_hrefs = [BASE_URL + h for h in all_hrefs if ('datum=' in h)]
+    def __init__(self, minimum: int, maximum: str, list_available: bool, source_id: str, cache_name: str = "devel"):
+        """
+        Initialize the data source
+        
+        :param manifest_url: URL template for the IIIF manifest
+        :param source_id: Source ID for the data source
+        :param project_id: Project ID for the data source
+        :param cache_name: Name of the cache for requests
+        """
+
+        self.min = minimum
+        self.max = maximum
+        self.list_available = list_available
+
+        self.base_url = f"https://{source_id}"
+        self.text_url = "{base_url}/cgi-content/annoshow?text={title_id}|{datum}|{page_number}"
+        self.image_url = "{base_url}/cgi-content/annoshow?call={title_id}|{datum}|{page_number}|{zoom_level}"
+
+        super().__init__(source_id=source_id, cache_name=cache_name)
     
-    all_ymd_hrefs = []
-    for yh in year_hrefs:
-        ah = utils.get_all_hrefs(yh, session=session)
-        all_ymd_hrefs.extend(h for h in ah if ('datum=' in h))
+    def fetch(self, title_id: str):
+        valid_datums = self._get_valid_datums(title_id)
+        
+        if self.min:
+            valid_datums = [d for d in valid_datums if d >= self.min]
+        if self.max:
+            valid_datums = [d for d in valid_datums if d <= self.max]
+        
+        if self.list_available:
+            valid_datums.sort()
+            for datum in valid_datums:
+                print(datum)
+            return
+        
+        folder = f"data/{self.source_id}/{title_id}"
+        os.makedirs(folder, exist_ok=True)
 
-    valid_datums = [utils.get_query_value(u, key='datum') for u in all_ymd_hrefs]
-    
-    valid_datums = list(set(valid_datums))
+        already = utils.list_directories(folder)
+        
+        for vd in tqdm.tqdm(valid_datums):
+            if vd not in already:
+                path_on_disk = self._get_text_for_datum(title_id, vd, page_number='x')
+                utils.split_anno_x_file(path_on_disk, folder + '/' + vd + '/txt')
+                utils.delete_file(path_on_disk)
+            else:
+                continue
 
-    return valid_datums
+    def _get_valid_datums(self, title_id: str):
+        title_url = self.base_url + f"/cgi-content/anno?apm=0&aid={title_id}"
+        all_hrefs = utils.get_all_hrefs(title_url, session=self.session)
+        year_hrefs = [f"{self.base_url}{h}" for h in all_hrefs if ('datum=' in h)]
 
-def get_text_for_datum(title_id, datum, page_number, session):
-    vd_uri = TEXT_URL.format(title_id=title_id, datum=datum, page_number='x')
-    folder = f"data/{SOURCE_ID}/{title_id}/{datum}/txt"
-    path_on_disk = folder + '/' + f"{SOURCE_ID}_{title_id}_{datum}.txt"
-    utils.download_remote_file(vd_uri, path=path_on_disk, session=session)
-    return path_on_disk
+        all_ymd_hrefs = []
+        for yh in year_hrefs:
+            ah = utils.get_all_hrefs(yh, session=self.session)
+            all_ymd_hrefs.extend(h for h in ah if ('datum=' in h))
 
-def main():
+        valid_datums = [utils.get_query_value(u, key='datum') for u in all_ymd_hrefs]
+        
+        valid_datums = list(set(valid_datums))
+
+        return valid_datums
+
+    def _get_text_for_datum(self, title_id, datum, page_number='x'):
+        vd_uri = self.text_url.format(base_url=self.base_url, title_id=title_id, datum=datum, page_number=page_number)
+        folder = f"data/{self.source_id}/{title_id}/{datum}/txt"
+        path_on_disk = folder + '/' + f"{self.source_id}_{title_id}_{datum}.txt"
+        utils.download_remote_file(vd_uri, path=path_on_disk, session=self.session)
+        return path_on_disk
+
+
+if __name__ == "__main__":
+    import argparse
+
+    SOURCE_ID = "anno.onb.ac.at"
+
     parser = argparse.ArgumentParser(description="Fetch text for a given title_id and optional date range.")
     parser.add_argument("title_id", type=str, help="The title ID to fetch data for.")
     parser.add_argument("--min", type=str, help="The minimum date in YYYYMMDD format.", default=None)
@@ -47,32 +99,5 @@ def main():
     
     args = parser.parse_args()
     
-    session = requests_cache.CachedSession("devel")
-    valid_datums = get_valid_datums(title_id=args.title_id, session=session)
-    
-    if args.min:
-        valid_datums = [d for d in valid_datums if d >= args.min]
-    if args.max:
-        valid_datums = [d for d in valid_datums if d <= args.max]
-    
-    if args.list_available:
-        valid_datums.sort()
-        for datum in valid_datums:
-            print(datum)
-        return
-    
-    folder = f"data/{SOURCE_ID}/{args.title_id}"
-    os.makedirs(folder, exist_ok=True)
-
-    already = utils.list_directories(folder)
-    
-    for vd in tqdm.tqdm(valid_datums):
-        if vd not in already:
-            path_on_disk = get_text_for_datum(title_id=args.title_id, datum=vd, page_number='x', session=session)
-            utils.split_anno_x_file(path_on_disk, folder + '/' + vd + '/txt')
-            utils.delete_file(path_on_disk)
-        else:
-            continue
-
-if __name__ == "__main__":
-    main()
+    data_source = AnnoDataSource(minimum=args.min, maximum=args.max, list_available=args.list_available, source_id=SOURCE_ID)
+    data_source.fetch(args.title_id)
--- fetcher/bsb.py
+++ fetcher/bsb.py
@@ -1,63 +1,79 @@
-import argparse
+from datasource import DataSource
+
 import logging
 import re
 
-import requests_cache
-import tqdm
-
 import utils 
 
-logging.basicConfig(level=logging.INFO)
+class BSBDataSource(DataSource):
+    """
+    Data source for BSB (Bayerische Staatsbibliothek) IDs.
+    """
 
-SOURCE_ID = "digipress.digitale-sammlungen.de"
-BASE_URL = "https://digipress.digitale-sammlungen.de"
-CALENDAR_URL = "https://digipress.digitale-sammlungen.de/calendar/newspaper/{title_id}"
+    def __init__(self, source_id: str, cache_name: str = "devel"):
+        """
+        Initialize the data source
+        :param source_id: Source ID for the data source
+        :param cache_name: Name of the cache for requests
+        """
+        self.source_id = source_id
+        self.base_url = f"https://{source_id}"
+        self.calendar_url = self.base_url + "/calendar/newspaper/{title_id}"
 
-def extract_bsb_id(url):
-    pattern = r'bsb\d+(_\d+)*_u\d+'
-    match = re.search(pattern, url)
-    if match:
-        return match.group(0)
-    else:
-        return None
+        super().__init__(source_id=source_id, cache_name=cache_name)
+        
+    def fetch(self, title_id: str):
+        calendar_hrefs = self._get_calendar_hrefs(title_id=title_id)
+        item_ids = self._get_item_ids_from_calendar_hrefs(calendar_hrefs)
 
-def get_calendar_hrefs(title_id, session):
-    calendar_url = CALENDAR_URL.format(title_id=title_id)
-    all_hrefs = utils.get_all_hrefs(calendar_url, session=session)
-    year_hrefs = [BASE_URL + h for h in all_hrefs if ('calendar' in h and title_id in h)]
-    all_calendar_hrefs = []
-    for yh in year_hrefs:
-        ah = utils.get_all_hrefs(yh, session=session)
-        all_calendar_hrefs.extend(BASE_URL + h for h in ah if ('calendar' in h and title_id in h))
-    
-    all_calendar_hrefs = list(set(all_calendar_hrefs))
+        item_ids.sort()
 
-    return all_calendar_hrefs
+        for item_id in item_ids:
+            print(item_id)
 
-def get_item_ids_from_calendar_hrefs(calendar_hrefs, session):
-    all_item_hrefs = []
-    
-    for ch in calendar_hrefs:
-        ah = utils.get_all_hrefs(ch, session=session)
-        all_item_hrefs.extend(BASE_URL + h for h in ah if ('view' in h))
+    def _extract_bsb_id(self, url) -> str | None:
+        pattern = r'bsb\d+(_\d+)*_u\d+'
+        match = re.search(pattern, url)
+        if match:
+            return match.group(0)
+        else:
+            return None
 
-    dedup_item_hrefs = list(set(all_item_hrefs))
-    return [extract_bsb_id(href) for href in dedup_item_hrefs]
+    def _get_calendar_hrefs(self, title_id) -> list[str]:
+        calendar_url = self.calendar_url.format(title_id=title_id)
+        all_hrefs = utils.get_all_hrefs(calendar_url, session=self.session)
+        year_hrefs = [self.base_url + h for h in all_hrefs if ('calendar' in h and title_id in h)]
+        all_calendar_hrefs = []
+        for yh in year_hrefs:
+            ah = utils.get_all_hrefs(yh, session=self.session)
+            all_calendar_hrefs.extend(self.base_url + h for h in ah if ('calendar' in h and title_id in h))
+        
+        all_calendar_hrefs = list(set(all_calendar_hrefs))
 
-def main():
-    parser = argparse.ArgumentParser(description="Fetch item_ids for a given BSB title_id, via calendar entry point.")
-    parser.add_argument("title_id", type=str, help="The title ID to fetch data for.")
-    
-    args = parser.parse_args()
-    
-    session = requests_cache.CachedSession("devel")
-    calendar_hrefs = get_calendar_hrefs(title_id=args.title_id, session=session)
-    item_ids = get_item_ids_from_calendar_hrefs(calendar_hrefs, session=session)
+        return all_calendar_hrefs
 
-    item_ids.sort()
-    for item_id in item_ids:
-        print(item_id)
-    return
+    def _get_item_ids_from_calendar_hrefs(self, calendar_hrefs) -> list[str]:
+        all_item_hrefs = []
+        
+        for ch in calendar_hrefs:
+            ah = utils.get_all_hrefs(ch, session=self.session)
+            all_item_hrefs.extend(self.base_url + h for h in ah if ('view' in h))
+
+        dedup_item_hrefs = list(set(all_item_hrefs))
+        hrefs = [self._extract_bsb_id(href) for href in dedup_item_hrefs]
+        return [h for h in hrefs if h is not None]
+
 
 if __name__ == "__main__":
-    main()
+    import argparse
+    logging.basicConfig(level=logging.INFO)
+
+    SOURCE_ID = "digipress.digitale-sammlungen.de"
+
+    parser = argparse.ArgumentParser(description="Fetch item_ids for a given BSB title_id, via calendar entry point.")
+    parser.add_argument("title_id", type=str, help="Title ID to fetch data for.")
+    args = parser.parse_args()
+
+    data_source = BSBDataSource(source_id=SOURCE_ID)
+    data_source.fetch(args.title_id)
+    
