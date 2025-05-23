2504de7d0ec001705df95763e8b4254f41896bfb
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2025-05-23 18:55:51 +0100

    Refactor `insert.py` such that its methods can be called from another Python script

--- fetcher/insert.py
+++ fetcher/insert.py
@@ -6,69 +6,13 @@ import time
 from tqdm import tqdm
 import typesense
 
-def wait_for_healthy(client):
-    while True:
-        try:
-            health = client.operations.is_healthy()
-            if health:
-                print("Typesense service is healthy.")
-                break
-        except Exception as e:
-            print(f"Waiting for Typesense service to be healthy: {e}")
-        time.sleep(2)
-
-def parse_args() -> argparse.Namespace:
+def fetch(client: typesense.Client, wait: bool = False):
     """
-    Parse command line arguments.
-    :return: Parsed arguments
-    :rtype: argparse.Namespace
+    Fetch data from JSONL file and load it into Typesense collection.
+    :param client: Typesense client
+    :param wait: Wait for Typesense service to be healthy before inserting data
     """
-
-    parser = argparse.ArgumentParser(description='Load JSONL data into a Typesense collection.')
-    
-    parser.add_argument('jsonl_file', type=str, help='Path to the JSONL file')
-    parser.add_argument('--batch-size', type=int, default=256, help='Number of documents to insert in each batch')
-    parser.add_argument('--wait-for-healthy', action='store_true', help='Wait for Typesense service to be healthy before inserting data')
-    parser.add_argument('--api-key', type=str, help='Typesense API key', default=os.getenv('TYPESENSE_API_KEY'))
-    parser.add_argument('--typesense-port', type=str, help='Typesense port', default=os.getenv('TYPESENSE_PORT'))
-    parser.add_argument('--typesense-protocol', type=str, help='Typesense protocol', default=os.getenv('TYPESENSE_PROTOCOL'))
-    parser.add_argument('--typesense-path', type=str, help='Typesense path', default=os.getenv('TYPESENSE_PATH'))
-    parser.add_argument('--typesense-fetcher-host', type=str, help='Typesense fetcher host', default=os.getenv('TYPESENSE_FETCHER_HOST'))
-    args = parser.parse_args()
-
-    if not args.api_key:
-        raise ValueError("TYPESENSE_API_KEY environment variable is not set or passed as --api-key")
-
-    if not args.typesense_port:
-        raise ValueError("TYPESENSE_PORT environment variable is not set or passed as --typesense-port")
-
-    if not args.typesense_protocol:
-        raise ValueError("TYPESENSE_PROTOCOL environment variable is not set or passed as --typesense-protocol")
-
-    if not args.typesense_path:
-        raise ValueError("TYPESENSE_PATH environment variable is not set or passed as --typesense-path")
-
-    if not args.typesense_fetcher_host:
-        raise ValueError("TYPESENSE_FETCHER_HOST environment variable is not set or passed as --typesense-fetcher-host")
-
-    return args
-
-def main():
-
-    args = parse_args()
-    
-    client = typesense.Client({
-        'nodes': [{
-            'host': args.typesense_fetcher_host,
-            'port': args.typesense_port,
-            'protocol': args.typesense_protocol,
-            'path': args.typesense_path
-        }],
-        'api_key': args.api_key,
-        'connection_timeout_seconds': 2
-    })
-
-    if args.wait_for_healthy:
+    if wait:
         wait_for_healthy(client)
 
     schema = {
@@ -108,5 +52,83 @@ def main():
         if batch:
             client.collections['documents'].documents.import_(batch)
 
+def wait_for_healthy(client):
+    while True:
+        try:
+            health = client.operations.is_healthy()
+            if health:
+                print("Typesense service is healthy.")
+                break
+        except Exception as e:
+            print(f"Waiting for Typesense service to be healthy: {e}")
+        time.sleep(2)
+
+def create_typesense_client(host, port, protocol, path, api_key):
+    """
+    Create and return a Typesense client instance
+    :param host: Typesense host
+    :param port: Typesense port
+    :param protocol: Typesense protocol
+    :param path: Typesense path
+    :param api_key: Typesense API key
+    """
+
+    client = typesense.Client({
+        'nodes': [{
+            'host': host,
+            'port': port,
+            'protocol': protocol,
+            'path': path
+        }],
+        'api_key': api_key,
+        'connection_timeout_seconds': 2
+    })
+    
+    return client
+
+def parse_args() -> argparse.Namespace:
+    """
+    Parse command line arguments.
+    :return: Parsed arguments
+    :rtype: argparse.Namespace
+    """
+
+    parser = argparse.ArgumentParser(description='Load JSONL data into a Typesense collection.')
+    
+    parser.add_argument('jsonl_file', type=str, help='Path to the JSONL file')
+    parser.add_argument('--batch-size', type=int, default=256, help='Number of documents to insert in each batch')
+    parser.add_argument('--wait-for-healthy', action='store_true', help='Wait for Typesense service to be healthy before inserting data')
+    parser.add_argument('--api-key', type=str, help='Typesense API key', default=os.getenv('TYPESENSE_API_KEY'))
+    parser.add_argument('--typesense-port', type=str, help='Typesense port', default=os.getenv('TYPESENSE_PORT'))
+    parser.add_argument('--typesense-protocol', type=str, help='Typesense protocol', default=os.getenv('TYPESENSE_PROTOCOL'))
+    parser.add_argument('--typesense-path', type=str, help='Typesense path', default=os.getenv('TYPESENSE_PATH'))
+    parser.add_argument('--typesense-fetcher-host', type=str, help='Typesense fetcher host', default=os.getenv('TYPESENSE_FETCHER_HOST'))
+    args = parser.parse_args()
+
+    if not args.api_key:
+        raise ValueError("TYPESENSE_API_KEY environment variable is not set or passed as --api-key")
+    if not args.typesense_port:
+        raise ValueError("TYPESENSE_PORT environment variable is not set or passed as --typesense-port")
+    if not args.typesense_protocol:
+        raise ValueError("TYPESENSE_PROTOCOL environment variable is not set or passed as --typesense-protocol")
+    if not args.typesense_path:
+        raise ValueError("TYPESENSE_PATH environment variable is not set or passed as --typesense-path")
+    if not args.typesense_fetcher_host:
+        raise ValueError("TYPESENSE_FETCHER_HOST environment variable is not set or passed as --typesense-fetcher-host")
+
+    return args
+
 if __name__ == '__main__':
-    main()
+
+    args = parse_args()
+
+    client = create_typesense_client(
+        host=args.typesense_fetcher_host,
+        port=args.typesense_port,
+        protocol=args.typesense_protocol,
+        path=args.typesense_path,
+        api_key=args.api_key
+    )
+
+    fetch(client, wait=args.wait_for_healthy)
+    print("Data loaded into Typesense collection 'documents'.")
