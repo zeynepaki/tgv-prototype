3cf4ba428788bd66b011c342c45f71e7ea9845a2
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2025-05-23 19:38:54 +0100

    Atomize Typesense args for use elsewhere

--- fetcher/insert.py
+++ fetcher/insert.py
@@ -94,17 +94,37 @@ def parse_args() -> argparse.Namespace:
     """
 
     parser = argparse.ArgumentParser(description='Load JSONL data into a Typesense collection.')
-    
+    parser = add_typesense_args(parser)
     parser.add_argument('jsonl_file', type=str, help='Path to the JSONL file')
     parser.add_argument('--batch-size', type=int, default=256, help='Number of documents to insert in each batch')
     parser.add_argument('--wait-for-healthy', action='store_true', help='Wait for Typesense service to be healthy before inserting data')
+    args = parser.parse_args()
+    validate_typesense_args(args)
+
+    if not os.path.exists(args.jsonl_file):
+        raise FileNotFoundError(f"JSONL file '{args.jsonl_file}' does not exist.")
+
+    return args
+
+def add_typesense_args(parser):
+    """
+    Add Typesense-related arguments to the argument parser.
+    :param parser: Argument parser
+    """
     parser.add_argument('--api-key', type=str, help='Typesense API key', default=os.getenv('TYPESENSE_API_KEY'))
     parser.add_argument('--typesense-port', type=str, help='Typesense port', default=os.getenv('TYPESENSE_PORT'))
     parser.add_argument('--typesense-protocol', type=str, help='Typesense protocol', default=os.getenv('TYPESENSE_PROTOCOL'))
     parser.add_argument('--typesense-path', type=str, help='Typesense path', default=os.getenv('TYPESENSE_PATH'))
     parser.add_argument('--typesense-fetcher-host', type=str, help='Typesense fetcher host', default=os.getenv('TYPESENSE_FETCHER_HOST'))
-    args = parser.parse_args()
 
+    return parser
+
+def validate_typesense_args(args):
+    """
+    Validate Typesense-related arguments.
+    :param args: Parsed arguments
+    """
+    
     if not args.api_key:
         raise ValueError("TYPESENSE_API_KEY environment variable is not set or passed as --api-key")
     if not args.typesense_port:
@@ -116,7 +136,6 @@ def parse_args() -> argparse.Namespace:
     if not args.typesense_fetcher_host:
         raise ValueError("TYPESENSE_FETCHER_HOST environment variable is not set or passed as --typesense-fetcher-host")
 
-    return args
 
 if __name__ == '__main__':
 
